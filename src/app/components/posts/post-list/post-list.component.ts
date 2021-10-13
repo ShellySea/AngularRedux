61e0d7487b136dc83e4fdbd807bf46f1060cf960
Saily J <30509037+ShellySea@users.noreply.github.com>
2021-10-14 01:09:55 +0530

    Extracting posts from store

--- src/app/components/posts/post-list/post-list.component.ts
+++ src/app/components/posts/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PostsI, PostInterface } from '../state/posts.state';
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts: PostInterface[];
+  constructor(private _store: Store<{ postStore: PostsI }>) { }
 
   ngOnInit() {
+    this._store.select('postStore').subscribe((data: PostsI) => {
+      console.log(data)
+      this.posts = data.posts
+    })
   }
 
 }
